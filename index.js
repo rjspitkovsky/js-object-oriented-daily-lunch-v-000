51f2e72f493a94ec4a6cea0e95a6524510ec0ccc
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-06 22:56:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -39,28 +39,6 @@ class Meal {
   }
 }
 
-
-// class Delivery{
-//   constructor(meal, customer){
-//     this.id = ++deliveryId;
-//     if (meal){ this.mealId = meal.id};
-//     if (customer){this.customerId = customer.id};
-//
-//     store.deliveries.push(this);
-//   }
-//   customer(){
-//     return store.customers.find(customer =>{
-//       return customer.id ===this.customerId;
-//     })
-//   }
-//   meal(){
-//     return store.meals.find(meal =>{
-//       return meal.id === this.mealId;
-//     })
-//   }
-// }
-
-// let deliveryId = 0
 class Delivery{
   constructor(meal, customer) {
     this.id = ++deliveryId;
