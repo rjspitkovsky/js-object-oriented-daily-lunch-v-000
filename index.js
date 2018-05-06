631e02d9c5d08f41e539870b671994030d80fef4
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-06 22:35:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 let store = {customers = [], meals = [], deliveries = [], employers =[]}
+
+let customerId = 0
+
+class Customer {
+  constructor(name, employer) {
+    this.name = name
+    this.id = ++customerId
+    store.customers.push(this)
+  }
+}
